bc41d36961bc598b31a15020ec7942af87fff777
alexeysvatov <zombeaver@gmail.com>
2022-12-02 22:00:57 +0500

    done tasks_6
    fixed new bugs from, great learning!

--- client_server_app/launcher_fedora.py
+++ client_server_app/launcher_fedora.py
@@ -10,7 +10,6 @@ import subprocess
 import sys
 from time import sleep
 
-
 PYTHON_PATH = sys.executable
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -22,20 +21,30 @@ def get_subprocess(file_with_args):
     return subprocess.Popen(args, preexec_fn=os.setpgrp)
 
 
-process = []
-while True:
-    TEXT_FOR_INPUT = "Выберите действие: q - выход, s - запустить сервер и клиенты, x - закрыть все окна: "
-    action = input(TEXT_FOR_INPUT)
+def main():
+    process = []
 
-    if action == "q":
-        break
-    elif action == "s":
-        process.append(get_subprocess("server.py"))
+    while True:
+        action = input(
+            'Выберите действие: q - выход , s - запустить сервер, k - запустить клиенты x - закрыть все окна:')
+        if action == 'q':
+            break
+        elif action == 's':
+            # Запускаем сервер!
+            process.append(get_subprocess("server.py"))
+        elif action == 'k':
+            print('Убедитесь, что на сервере зарегистрировано необходимо количество клиентов с паролем 123456.')
+            print('Первый запуск может быть достаточно долгим из-за генерации ключей!')
+            clients_count = int(
+                input('Введите количество тестовых клиентов для запуска: '))
+            # Запускаем клиентов:
+            for i in range(1, clients_count + 1):
+                process.append(get_subprocess(f"client.py -n test{i} -p 123456"))
+        elif action == 'x':
+            while process:
+                victim = process.pop()
+                os.killpg(victim.pid, signal.SIGINT)
 
-        for i in range(2):
-            process.append(get_subprocess(f"client.py -n test{i+1}"))
 
-    elif action == "x":
-        while process:
-            victim = process.pop()
-            os.killpg(victim.pid, signal.SIGINT)
+if __name__ == '__main__':
+    main()
